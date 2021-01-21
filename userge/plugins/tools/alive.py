48ad942881193d443c88b7d7770b357f4ff349b3
X-Newbie <69149585+X-Newbie@users.noreply.github.com>
2021-01-21 22:48:28 +0700

    Add files via upload

--- userge/plugins/tools/alive.py
+++ userge/plugins/tools/alive.py
@@ -64,21 +64,23 @@ def _get_mode() -> str:
 
 def _get_alive_text_and_markup(message: Message) -> Tuple[str, Optional[InlineKeyboardMarkup]]:
     markup = None
-    output = f"""
-**⏱ Uptime** : `{userge.uptime}`
-**💡 Version** : `{get_version()}`
+    output = f"""X-USERGE is running..\n
+▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n
+**⌚ uptime** : `{userge.uptime}`
+**🛠️ version** : `{get_version()}`
 **⚙️ Mode** : `{_get_mode().upper()}`
-
-• **Sudo**: `{_parse_arg(Config.SUDO_ENABLED)}`
-• **Pm-Guard**: `{_parse_arg(not Config.ALLOW_ALL_PMS)}`
-• **Anti-Spam**: `{_parse_arg(Config.ANTISPAM_SENTRY)}`"""
+\n╭━━━━━━━━━━━━━━━━╮
+┣[• **Sudo**: `{_parse_arg(Config.SUDO_ENABLED)}`🔥
+┣[• **Pm-Guard**: `{_parse_arg(not Config.ALLOW_ALL_PMS)}`🔥
+┣[• **Anti-Spam**: `{_parse_arg(Config.ANTISPAM_SENTRY)}`🔥"""
     if Config.HEROKU_APP:
-        output += f"\n• **Dyno-saver**: `{_parse_arg(Config.RUN_DYNO_SAVER)}`"
+        output += f"\n┣[• **Dyno-saver**: `{_parse_arg(Config.RUN_DYNO_SAVER)}`🔥"
     output += f"""
-• **Unofficial**: `{_parse_arg(Config.LOAD_UNOFFICIAL_PLUGINS)}`
-
-    **__Python__**: `{versions.__python_version__}`
-    **__Pyrogram__**: `{versions.__pyro_version__}`"""
+┣[• **Unofficial**: `{_parse_arg(Config.LOAD_UNOFFICIAL_PLUGINS)}`🔥
+╰━━━━━━━━━━━━━━━━╯\n
+  ⚙️**__Python__**: `{versions.__python_version__}`
+  ⚙️**__Pyrogram__**: `{versions.__pyro_version__}`
+\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱"""
     if not message.client.is_bot:
         output += f"""\n
 🎖 **{versions.__license__}** | 👥 **{versions.__copyright__}** | 🧪 **[Repo]({Config.UPSTREAM_REPO})**
